c6162857f87e27791cae1a39a217af4fac7fba15
AngeSilk <angesilk007@gmail.com>
2024-04-25 16:57:54 -0300

    v0.2

--- clienteMqtt/clienteMqtt.py
+++ clienteMqtt/clienteMqtt.py
@@ -14,36 +14,49 @@ async def contador():
 async def timestamp():
    return time.time()
 
-async def mqtt():
+async def distributor(client, topico, subtopicos):
+
+    async for message in client.messages:
+        if message.topic.matches(topico + "/"+ subtopicos[0]):
+            logging.info(message.payload)
+            contador_queue.put_nowait(message)
+        elif message.topic.matches(topico + "/"+ subtopicos[1]):
+            logging.info(message.payload)
+            timestamp_queue.put_nowait(message)
+
+contador_queue = asyncio.Queue()
+timestamp_queue = asyncio.Queue()
+
+async def main():
     
+    topico = os.environ['TOPICO']
+    subtopicos = str(os.getenv("TOPICOS")).split(",")
+    server = os.environ['SERVIDOR']
+    
+    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    tls_context.verify_mode = ssl.CERT_REQUIRED
+    tls_context.check_hostname = True
+    tls_context.load_default_certs()
+
     async with aiomqtt.Client(
         server,
         port=8883,
         tls_context=tls_context
     ) as client:
-        logging.info(subtopicos)
-        for topic in subtopicos:
-            await client.subscribe(topico + "/"+ topic)
-        async for message in client.messages:
-            logging.info(str(message.topic) + ": " + message.payload.decode("utf-8"))
+        try:
+            logging.info(str(subtopicos))
+            for topic in subtopicos:
+                await client.subscribe(topico + "/"+ topic)
 
-async def main():
-    tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    tls_context.verify_mode = ssl.CERT_REQUIRED
-    tls_context.check_hostname = True
-    tls_context.load_default_certs()
-    logging.info(subtopicos)
-    
-    topico = os.environ['TOPICO']
-    subtopicos = str(os.getenv("TOPICOS")).split(",")
-    server = os.environ['SERVIDOR']
+            async with asyncio.TaskGroup() as tg:
+                tg.create_task(distributor(client, topico, subtopicos))
+                tg.create_task(contador())
+                tg.create_task(timestamp())
 
-    async with asyncio.TaskGroup() as tg:
-        task1 = tg.create_task(contador())
-        task2 = tg.create_task(timestamp())
-        task3 = tg.create_task(mqtt)
+        except KeyboardInterrupt:
+            logging.info("Detetindo por el ususario")
+        except Exception as e:
+            logging.error(f"Se ha producido un error: {str(e)}")
 
-    logging.info(task1.result())
-    
 if __name__ == "__main__":
     asyncio.run(main())
